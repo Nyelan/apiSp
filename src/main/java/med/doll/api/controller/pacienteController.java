f216c1e3e237e056253205b7b6e141c3419727c4
Nyelan <leonardovazgremista@hotmail.com>
2023-09-28 14:55:43 -0300

    Atualizadas as pastas: - pacienteController.java Adicionando o metodo Get Criadas as pastas: - DadosListagemPacientes.java;

--- src/main/java/med/doll/api/controller/pacienteController.java
+++ src/main/java/med/doll/api/controller/pacienteController.java
@@ -1,15 +1,18 @@
 package med.doll.api.controller;
 
 import jakarta.validation.Valid;
+import med.doll.api.medico.DadosListagemMedicos;
+import med.doll.api.paciente.DadosListagemPacientes;
 import med.doll.api.paciente.Paciente;
 import med.doll.api.paciente.dadosCadastroPaciente;
 import med.doll.api.paciente.pacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -22,4 +25,12 @@ public class pacienteController {
     public void cadastrar(@RequestBody @Valid dadosCadastroPaciente dados){
         repository.save(new Paciente(dados));
     }
+
+    @GetMapping
+    public Page<DadosListagemPacientes> listar(@PageableDefault(sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPacientes::new);
+    }
+
+
+
 }
